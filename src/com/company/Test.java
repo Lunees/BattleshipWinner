ec45fe5637ad1c2c1109dabad374ec7aa294a1f7
JesperJohansson95 <92728289+JesperJohansson95@users.noreply.github.com>
2021-10-20 20:33:00 +0200

    Tog bort sög.
    Kom igen nu Ralle vakna förfan!
    
    Co-Authored-By: rasmus wahlström <71094850+Lunees@users.noreply.github.com>

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,8 +3,6 @@ package com.company;
 public class Test {
     public static void testmetod(){
         System.out.println("Testtest");
-    }
-    public static void tjena(){
-        easdf
+
     }
 }
