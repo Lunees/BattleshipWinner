38623d63e025e079b6ba95af9a5545b7d64576b0
isabellesjlund <92785457+isabellesjlund@users.noreply.github.com>
2021-10-26 11:09:46 +0200

    Skapade skeppobjekt
    Uppdaterade skeppkonstruktorn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         GameBoard gameBoard = new GameBoard(0, 0, new int[10][10]);
         // A = Hangar B = Slagskepp C = Kryssare D = Ubåt
-        Ship ship = new Ship('A', 5, true); // Skapar skeppobjekt
+        Ship ship = new Ship( "Carrier",'A', 5, true); // Skapar skeppobjekt
+        Ship ship2 = new Ship("Battleship",'B',4,true);
+        Ship ship3 = new Ship("Cruiser",'C',3,true);
+        Ship ship4 = new Ship("Submarine",'D',2,true);
 
     }
 }
--- src/com/company/Ship.java
+++ src/com/company/Ship.java
@@ -1,15 +1,26 @@
 package com.company;
 
 public class Ship {
+    private String shipName;
     private char typeOfShip;
     private int length;
     private boolean alive = true;
 
 
-    public Ship(char typeOfShip, int length, boolean alive) {
+
+    public Ship(String shipName, char typeOfShip, int length, boolean alive) {
+        this.shipName = shipName;
         this.typeOfShip = typeOfShip;
         this.length = length;
         this.alive = alive;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
     }
 
     public char getTypeOfShip() {
@@ -35,4 +46,5 @@ public class Ship {
     public void setAlive(boolean alive) {
         this.alive = alive;
     }
+
 }
