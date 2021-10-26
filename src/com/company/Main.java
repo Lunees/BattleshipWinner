42c22dafda60fb74b2dd9f065e34e5a9ebfc9d7a
JesperJohansson95 <92728289+JesperJohansson95@users.noreply.github.com>
2021-10-26 10:32:04 +0200

    Skapade objekt
    Skapade objekt-ship/Gameboard i klassen Main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        GameBoard gameBoard = new GameBoard(0, 0, new int[10][10]);
+        // A = Hangar B = Slagskepp C = Kryssare D = Ubåt
+        Ship ship = new Ship('A', 5, true); // Skapar skeppobjekt
+
     }
 }
