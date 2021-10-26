2eb9257016fadfd0332824fb620bd87e19282f88
elkadoR <jonas.everettson@edu.edugrade.se>
2021-10-26 10:01:13 +0200

    Made gameboard
    skapade ett gameboard med höjd och längd , getters / setters + array bräda

---
+++ src/com/company/GameBoard.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class GameBoard {
+
+    private int height;
+    private int width;
+
+    private int [] [] playerBoard = new int[10][10];
+
+    
+
+
+
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int[][] getPlayerBoard() {
+        return playerBoard;
+    }
+
+    public void setPlayerBoard(int[][] playerBoard) {
+        this.playerBoard = playerBoard;
+    }
+}
