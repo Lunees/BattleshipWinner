ff540a8e22cbd35f791882993f764c08719dc90e
Sara Nilsson <nilssonsara97@gmail.com>
2021-10-27 14:57:47 +0200

    Tog bort placementmetoder
    Placement metoder flyttade till placement klassen

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -1,18 +1,21 @@
 package com.company;
 import static java.lang.Math.*;
 public class GameBoard {
-
+    //Variabler
     private int rows = 10;
     private int columns = 10;
 
     private int[][] playerBoard = new int[rows][columns];
 
+    //Constructor
     public GameBoard(int rows, int columns, int[][] playerBoard) {
         this.rows = rows;
         this.columns = columns;
         this.playerBoard = playerBoard;
     }
 
+    //Metoder
+    //För att skapa ett tomt spelbräde
     public void createGameBoard(){
         for(int i = 0; i < rows; i++ ){
             for (int j = 0; j < columns; j++){
@@ -22,6 +25,7 @@ public class GameBoard {
         showGameBoard(); 
     }
 
+    //Visar spelbrädet
     public void showGameBoard(){
         for (int i = 0; i < 10; i++){
 
@@ -31,31 +35,8 @@ public class GameBoard {
             System.out.println();
         }
     }
-     // Y-Led upp , X-Led -
 
-public void placeHorizontal(Ship ship){
-    int startIndexX = (int)(Math.random()*5); //slänger ut skeppet vertikalt
-    int startIndexY = (int)(Math.random()*10); // slänger ut de random horizontalt
-
-        for(int i = 0; i < ship.getLength(); i++){
-          playerBoard[startIndexY][startIndexX+i] = ship.getTypeOfShip();
-            System.out.println(i);
-        }
-    }
-public void placeVertical(Ship ship){
-    int startIndexY = (int)(Math.random()*5); //slänger ut skeppet
-    int startIndexX = (int)(Math.random()*10); // slänger ut de random horizontalt
-
-        for(int i = 0; i < ship.getLength(); i++){
-            playerBoard[startIndexY+i][startIndexX] = ship.getTypeOfShip();
-            System.out.println(i);
-        }
-    }
-
-
-
-
-
+    //Get n Set
     public int getRows() {
         return rows;
     }
