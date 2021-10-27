0b601873401730ab619c9d2e1f49837780919f1a
elkadoR <jonas.everettson@edu.edugrade.se>
2021-10-27 14:08:44 +0200

    Update main
    La till vericalt skepp i main och hämtade även in Placement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,23 @@ public class Main {
     public static void main(String[] args) {
         GameBoard gameBoard = new GameBoard(10, 10, new int[10][10]);
         gameBoard.createGameBoard();
+        Placement placement = new Placement();
+        placement.setGameBoard(gameBoard);
+
+
         // 5 = Carrier 4 = Battleship 3 = Cruiser 2 = Submarine
         Ship ship = new Ship( "Carrier",5, 5, true); // Skapar skeppobjekt
         Ship ship2 = new Ship("Battleship",4,4,true);
         Ship ship3 = new Ship("Cruiser",3,3,true);
         Ship ship4 = new Ship("Submarine",2,2,true);
+
         gameBoard.placeHorizontal(ship);
         System.out.println(" ");
+        gameBoard.placeVertical(ship2);
+        System.out.println(" ");
         gameBoard.showGameBoard();
+
+
 
     }
 }
