4de7f0f0d18efc8ea1741afe6b8fc1343466afc3
Sara Nilsson <nilssonsara97@gmail.com>
2021-10-29 23:19:00 +0200

    Skapade GameFunktion
    Skapade klass för att skjuta och bli skjuten

---
+++ src/com/company/GameFunction.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class GameFunction {
+    //Variabler
+    GameBoard gameBoard;
+
+    //Konstruktorer
+    public GameFunction() {
+    }
+
+    public GameFunction(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    //Metoder
+    public boolean gettingShot (int row, int column){
+        if (gameBoard.getPlayerBoard()[row][column] != 0) {
+            gameBoard.getPlayerBoard()[row][column] = -1;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public String shooting (){
+
+        return "A 3";
+    }
+
+    //Get n set
+
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
+
+    public void setGameBoard(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+}
