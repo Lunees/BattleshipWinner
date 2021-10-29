4d77a0edfdab6e86827cb096b4ae3897a5586b48
Sara Nilsson <nilssonsara97@gmail.com>
2021-10-29 23:17:33 +0200

    Placering manuellt
    Överlagra metoder i Placement så att man kan placera skepp manuellt

--- src/com/company/Placement.java
+++ src/com/company/Placement.java
@@ -9,7 +9,7 @@ public class Placement {
     //Placera vertikalt eller horisontellt
     public void placeHorizontal(Ship ship){
         int startIndexX = (int)(Math.random()*(10 - ship.getLength())); //slänger ut skeppet vertikalt, inte för nära kanten
-        int startIndexY = (int)(Math.random()*10); // slänger ut de random horisontalt
+        int startIndexY = (int)(Math.random()*10); //slänger ut de random horisontalt
 
         //Placerar ut skepp
         for(int i = 0; i < ship.getLength(); i++){
@@ -19,7 +19,27 @@ public class Placement {
 
     public void placeVertical(Ship ship){
         int startIndexY = (int)(Math.random() * (10 - ship.getLength())); //slänger ut skeppet horisontalt, inte för nära kanten
-        int startIndexX = (int)(Math.random() * 10); // slänger ut de random vertikalt
+        int startIndexX = (int)(Math.random() * 10); //slänger ut de random vertikalt
+
+        //Placerar ut skepp
+        for(int i = 0; i < ship.getLength(); i++){
+            gameBoard.getPlayerBoard()[startIndexY + i][startIndexX] = ship.getTypeOfShip();
+        }
+    }
+
+    public void placeHorizontal(Ship ship, int row, int column){
+        int startIndexX = column; //slänger ut skeppet vertikalt, inte för nära kanten
+        int startIndexY = row; //slänger ut de random horisontalt
+
+        //Placerar ut skepp
+        for(int i = 0; i < ship.getLength(); i++){
+            gameBoard.getPlayerBoard()[startIndexY][startIndexX + i] = ship.getTypeOfShip();
+        }
+    }
+
+    public void placeVertical(Ship ship, int row, int column){
+        int startIndexY = row; //slänger ut skeppet horisontalt
+        int startIndexX = column; //slänger ut skeppet vertikalt
 
         //Placerar ut skepp
         for(int i = 0; i < ship.getLength(); i++){
