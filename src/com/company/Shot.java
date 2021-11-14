277f9dccafde34442a3157c674499901ed4bf2b2
Sara Nilsson <nilssonsara97@gmail.com>
2021-11-14 01:28:42 +0100

    Create Shot.java
    Skapade klass för att hålla koll på skott

---
+++ src/com/company/Shot.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Shot {
+    //Variabler
+    private int indexRow,
+                indexColumn;
+    private char hitOrMiss;
+
+    //Konstruktorer
+    public Shot(int indexRow, int indexColumn, char hitOrMiss) {
+        this.indexRow = indexRow;
+        this.indexColumn = indexColumn;
+        this.hitOrMiss = hitOrMiss;
+    }
+
+    public Shot(int indexRow, int indexColumn) {
+        this.indexRow = indexRow;
+        this.indexColumn = indexColumn;
+    }
+
+    //Get n set
+    public int getIndexRow() {
+        return indexRow;
+    }
+
+    public void setIndexRow(int indexRow) {
+        this.indexRow = indexRow;
+    }
+
+    public int getIndexColumn() {
+        return indexColumn;
+    }
+
+    public void setIndexColumn(int indexColumn) {
+        this.indexColumn = indexColumn;
+    }
+
+    public char getHitOrMiss() {
+        return hitOrMiss;
+    }
+
+    public void setHitOrMiss(char hitOrMiss) {
+        this.hitOrMiss = hitOrMiss;
+    }
+}
